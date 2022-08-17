1f1d32da7783c35c41b7f3477decac5bb8fdf9f1
leeturn <liketodeath@gmail.com>
2022-08-17 19:08:32 +0900

    refactor: use spring DI

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,13 +2,21 @@ package moviebuddy;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MovieBuddyFactory {
     @Bean
-    public MovieFinder movieFinder() {
-        return new MovieFinder(new CsvMovieReader());
+    public MovieReader movieReader() {
+        return new CsvMovieReader();
+    }
+    @Bean
+    public MovieFinder movieFinder(MovieReader movieReader) {
+        return new MovieFinder(movieReader);
     }
 }
